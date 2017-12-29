f4f8de0c0eb1906a6f578aea74cfd1e613ad04f1
WhiteAce <whiteace@yeah.net>
2017-12-29 11:00:27 +0800

    standard

--- containers/app.js
+++ containers/app.js
@@ -1,55 +1,51 @@
-import React from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import Login from '../pages/account/login';
-import Slider from '../pages/slider/index';
-import Boot from '../components/boot';
-import Tabs from './tabs';
-import * as appActions from '../actions/app';
+import React from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import Login from '../pages/account/login'
+import Slider from '../pages/slider/index'
+import Boot from '../components/boot'
+import Tabs from './tabs'
+import * as appActions from '../actions/app'
 
 import Repast from '../Repast'
-// import { AsyncStorage } from 'react-native';
+// import { AsyncStorage } from 'react-native'
 
-// AsyncStorage.multiRemove(['booted', 'user', 'logined', 'entered']);
+// AsyncStorage.multiRemove(['booted', 'user', 'logined', 'entered'])
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+  componentDidMount () {
+    this.props.willEnterApp()
+  }
+
+  render () {
+    if (!this.props.booted) {
+      return <Boot {...this.props} />
     }
 
-    componentDidMount() {
-        this.props.willEnterApp();
+    if (!this.props.entered) {
+      return <Slider {...this.props} />
     }
 
-    render() {
-        if (!this.props.booted) {
-            return <Boot {...this.props} />;
-        }
-
-        if (!this.props.entered) {
-            return <Slider {...this.props} />;
-        }
-
-        if (!this.props.logined) {
-            return <Login {...this.props} />;
-        }
-
-        return <Tabs />;
+    if (!this.props.logined) {
+      return <Login {...this.props} />
     }
+
+    return <Tabs />
+  }
 }
 
-function mapStateToProps(state) {
-    return {
-        booted: state.get('app').booted,
-        logined: state.get('app').logined,
-        entered: state.get('app').entered,
-        banners: state.get('app').banners,
-        popup: state.get('app').popup
-    };
+function mapStateToProps (state) {
+  return {
+    booted: state.get('app').booted,
+    logined: state.get('app').logined,
+    entered: state.get('app').entered,
+    banners: state.get('app').banners,
+    popup: state.get('app').popup
+  }
 }
 
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators(appActions, dispatch);
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators(appActions, dispatch)
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App)
