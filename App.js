4b3dfcbcaba29fc257e9d1a85923f13bed215e11
WhiteAce <whiteace@yeah.net>
2018-03-16 14:39:35 +0800

    ...

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 // import AppContainer from './containers/app'
-// import Player from './components/Player'
-import StickyHeaderExample from './components/StickyHeader/example'
+import Player from './components/Player'
+// import StickyHeaderExample from './components/StickyHeader/example'
 import configureStore from './store'
 import { StyleSheet, View } from 'react-native'
 const store = configureStore()
@@ -12,8 +12,8 @@ export default class App extends React.Component {
       <Provider store={store}>
         <View style={styles.container}>
           {/* <AppContainer /> */}
-          {/* <Player source={require('./static/movie.mp4')} /> */}
-          <StickyHeaderExample />
+          <Player source={require('./static/movie.mp4')} />
+          {/* <StickyHeaderExample /> */}
         </View>
       </Provider>
     )
